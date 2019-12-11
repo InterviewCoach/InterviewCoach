24e2f328e60c8efade2dbcc0acdcfb93b390a0a6
Malka <Malka.in.red@gmail.com>
2019-12-11 11:01:37 -0500

    cleaup History component

--- InterviewCoach/components/HistorySessionScreen.js
+++ InterviewCoach/components/HistorySessionScreen.js
@@ -1,48 +1,34 @@
 import React from 'react';
 import axios from 'axios';
 import Splash from './Splash'
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  ScrollView,
-  SafeAreaView,
-  Button,
-  Image
-} from 'react-native';
-import {
-  VictoryBar,
-  VictoryChart,
-  VictoryTheme,
-  VictoryAxis,
-  VictoryPie,
-  VictoryLabel,
-} from 'victory-native';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, SafeAreaView, Image } from 'react-native';
+import { VictoryBar, VictoryChart, VictoryTheme, VictoryAxis } from 'victory-native';
 import Constants from 'expo-constants';
 import hamburger from '../assets/hamburgerBlack.png';
 
+// History component where a user cna see data visualizations of their past interview sessions
 class HistorySessionScreen extends React.Component {
   constructor() {
     super();
     this.state = {
       sessions: [],
-      // colors: ['gold', '#B0E0E6', '#20B2AA', '#DDA0DD'],
+      got: false
     };
   }
+
   async componentDidMount() {
     await this.loadSessionData();
   }
 
+  // get users history data and put it on state
   loadSessionData = async () => {
     try {
       const { data } = await axios.get(
         'https://interview-coach-server.herokuapp.com/api/sessions/latest/10'
       );
-      console.log('sessions data->', data);
-
       this.setState({
         sessions: data,
+        got: true
       });
     } catch (error) {
       console.error(error);
@@ -50,63 +36,26 @@ class HistorySessionScreen extends React.Component {
   };
 
   render() {
-    if (!this.state.sessions.length)
+    // render spalsh while getting data from backend
+    if (!this.state.sessions.length && !this.state.got)
       return <Splash message={'loading your history...'} />
+    
+    // render graphs when data is recieved
     return (
       <View style={styles.container}>
 
+        {/* drawer navigation */}
         <TouchableOpacity style={styles.burger} onPress={this.props.navigation.toggleDrawer}>
           <Image source={hamburger} />
         </TouchableOpacity>
 
+        {/* Data visualization of users session history */}
         <Text style={styles.title}>SESSION HISTORY</Text>
         <SafeAreaView style={styles.scrollContainer}>
           <ScrollView style={styles.scrollView}>
             <View style={styles.chartContainer}>
               {this.state.sessions.map((session, idx) => {
                 return (
-                  // <VictoryPie
-                  //   key={session.id}
-                  //   data={[
-                  //     {
-                  //       x: 'actually',
-                  //       y: Math.round(
-                  //         (session.actuallyWordCount / session.totalWordCount) *
-                  //         100
-                  //       ),
-                  //     },
-                  //     {
-                  //       x: 'like',
-                  //       y: Math.round(
-                  //         (session.likeWordCount / session.totalWordCount) * 100
-                  //       ),
-                  //     },
-                  //     {
-                  //       x: 'basically',
-                  //       y: Math.round(
-                  //         (session.basicallyWordCount / session.totalWordCount) *
-                  //         100
-                  //       ),
-                  //     },
-                  //     {
-                  //       x: 'other',
-                  //       y: Math.round(
-                  //         ((session.totalWordCount -
-                  //           (session.actuallyWordCount +
-                  //             session.likeWordCount +
-                  //             session.basicallyWordCount)) /
-                  //           session.totalWordCount) *
-                  //         100
-                  //       ),
-                  //     },
-                  //   ]}
-                  //   labels={({ datum }) => `${datum.x}: ${datum.y}%`}
-                  //   colorScale={['gold', '#B0E0E6', '#20B2AA', '#DDA0DD']}
-                  //   padding={{ left: 100, right: 100 }}
-                  //   style={{ labels: { fontSize: 10, fill: 'black' } }}
-                  //   labelComponent={<VictoryLabel angle={325} />}
-                  // />
-
                   <VictoryChart
                     key={session.id}
                     width={350}
@@ -139,39 +88,16 @@ class HistorySessionScreen extends React.Component {
                         },
                       }}
                       categories={{
-                        x: [`like`, `actually`, `basically`],
-                        y: [
-                          `1`,
-                          `2`,
-                          `3`,
-                          `4`,
-                          `5`,
-                          '6',
-                          '7',
-                          '8',
-                          '9',
-                          '10',
-                        ],
-
+                        x: ['like', 'actually', 'basically'],
+                        y: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
                       }}
                     />
                   </VictoryChart>
-
                 );
               })}
             </View>
           </ScrollView>
         </SafeAreaView>
-        {/* <View>
-          <TouchableOpacity style={styles.buttonContainer}>
-            <Text
-              style={styles.buttonText}
-              onPress={() => this.props.navigation.navigate('InSession')}
-            >
-              NEW SESSION
-            </Text>
-          </TouchableOpacity>
-        </View> */}
       </View>
     );
   }
